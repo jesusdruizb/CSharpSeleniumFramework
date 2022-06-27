0898f78148e52f4afaa23659c7daa7d8fd67e2f8
jesusdruizb <jesusdruizb@protonmail.com>
2022-06-26 18:15:35 -0600

    no message

--- UITestProject/Tests/BaseTestSuite.cs
+++ UITestProject/Tests/BaseTestSuite.cs
@@ -1,7 +1,9 @@
+using System;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 using WebDriverManager.Helpers;
@@ -14,38 +16,71 @@ namespace UITestProject.Tests
     {
         public IWebDriver WebDriver { get; set; }
         public string BrowserType { get; set; }
-        
-        
+
+        [SetUp]
+        public void BrowserInit()
+        {
+            BuildBrowser("remote","firefox");
+        }
+        public void BuildBrowser(string type, string browserType)
+        {
+            if (type.ToLower().Equals("local"))
+            {
+                switch (browserType.ToLower())
+                {
+                    case "chrome":
+                        ChromeDriverSetup();
+                        break;
+                    case "firefox":
+                        FirefoxDriverSetup();
+                        break;
+                    default:
+                       throw new ArgumentException("Unknown browser type, chose Chrome or Firefox");
+                }
+            }
+            else if (type.ToLower().Equals("remote"))
+            {
+                RemoteDriverSetup(browserType);
+            }
+            else
+            {
+                throw new ArgumentException($"{type} of test invalid, chose local or remote");
+            }
+        }
+
         public void ChromeDriverSetup()
         {
             new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
             //new DriverManager().SetUpDriver(new ChromeConfig());
             WebDriver = new ChromeDriver();
         }
-        /*
-        public void ReadBrowserType()
-        {
-            BrowserType = TestContext.Parameters.Get("browser", "");
-            switch (BrowserType)
-            {
-                case "Chrome":
-                    ChromeDriverSetup();
-                    break;
-                case "Firefox":
-                    FirefoxDriverSetup();
-                    break;
-                default:
-                    Console.WriteLine("Unknown browser type");
-                    break;
-            }
-        }
-        */
-        [SetUp]
+        
         public void FirefoxDriverSetup()
         {
             new DriverManager().SetUpDriver(new FirefoxConfig());
             WebDriver = new FirefoxDriver();
         }
+        
+        public void RemoteDriverSetup(string browserType)
+        {
+            Uri DOCKER_GRID_URI = new Uri("http://localhost:4444/wd/hub");
+
+            switch (browserType.ToLower())
+            {
+                case "chrome":
+                    ChromeOptions chromeOptions = new ChromeOptions{BrowserVersion= "", PlatformName  ="Linux"};
+                    chromeOptions.AddArgument("--start-maximized");
+                    WebDriver = new RemoteWebDriver(DOCKER_GRID_URI, chromeOptions.ToCapabilities());
+                    break;
+                case "firefox":
+                    FirefoxOptions ffOptions = new FirefoxOptions{BrowserVersion= "", PlatformName  ="Linux"};
+                    ffOptions.AddArgument("--start-maximized");
+                    WebDriver = new RemoteWebDriver(DOCKER_GRID_URI, ffOptions.ToCapabilities());
+                    break;
+                default:
+                    throw new ArgumentException("Unknown browser type, chose Chrome or Firefox");
+            }
+         }
         
         [TearDown]
         public void DriverTearDown()
